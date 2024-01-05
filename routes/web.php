4027b16dee36c7071aa4c7e761c4cf7c3df6d10d
Yuri de Oliveira <jarvis@pop-os.localdomain>
2024-01-05 16:10:25 -0300

    HomeController is now an invokable controller

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,5 @@ use App\Http\Controllers\CountryController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [HomeController::class, 'index'])->name('home.index');
-Route::resource('countries', CountryController::class)->except(['index']);
+Route::get('/', HomeController::class)->name('home.index');
+Route::resource('countries', CountryController::class)->except('index');
